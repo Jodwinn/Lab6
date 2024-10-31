0a7f4c3319eb2cf898c63867d9edb5a7f2c6d5e8
Jodwinn <pavelkharkovski@gmail.com>
2024-10-31 17:51:29 +0200

    Update .cpp

--- .cpp
+++ .cpp
@@ -1,1 +1,38 @@
+#include <windows.h>;
+#include <iostream>
+using namespace std;
 
+int main() {
+    SetConsoleOutputCP(1251);
+    SetConsoleCP(1251);
+    int size;
+
+    cout << "Введіть розмір масиву: ";
+    cin >> size;
+
+    int* arr = new int[size];
+    cout << "Введіть елементи масиву:" << endl;
+    for (int i = 0; i < size; i++) {
+        cout << "arr[" << i << "]: ";
+        cin >> arr[i];
+    }
+
+    int* minznach = arr;
+    int* maxznach = arr;
+
+    for (int i = 1; i < size; i++) {
+        if (*(arr + i) < *minznach) {
+            minznach = arr + i;
+        }
+        if (*(arr + i) > *maxznach) {
+            maxznach = arr + i;
+        }
+    }
+
+    cout << "Мінімальне значення в масиві: " << *minznach << endl;
+    cout << "Максимальне значення в масиві: " << *maxznach << endl;
+
+    delete[] arr;
+
+    return 0;
+}
